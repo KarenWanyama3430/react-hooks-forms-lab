c52816ef82ec050127914dbd7edfe59921bbcf83
+ <+>
2022-07-21 00:21:20 +0300

    commit

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,48 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ItemForm from "../components/ItemForm";
+import App from "../components/App";
 
-function ItemForm(props) {
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
 
-      <label>
-        Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
+test("calls the onItemFormSubmit callback prop when the form is submitted", () => {
+  const onItemFormSubmit = jest.fn();
+  render(<ItemForm onItemFormSubmit={onItemFormSubmit} />);
 
-      <button type="submit">Add to List</button>
-    </form>
+  fireEvent.change(screen.queryByLabelText(/Name/), {
+    target: { value: "Ice Cream" },
+  });
+
+  fireEvent.change(screen.queryByLabelText(/Category/), {
+    target: { value: "Dessert" },
+  });
+
+  fireEvent.submit(screen.queryByText(/Add to List/));
+
+  expect(onItemFormSubmit).toHaveBeenCalledWith(
+    expect.objectContaining({
+      id: expect.any(String),
+      name: "Ice Cream",
+      category: "Dessert",
+    })
   );
-}
+});
 
-export default ItemForm;
+test("adds a new item to the list when the form is submitted", () => {
+  render(<App />);
+
+  const dessertCount = screen.queryAllByText(/Dessert/).length;
+
+  fireEvent.change(screen.queryByLabelText(/Name/), {
+    target: { value: "Ice Cream" },
+  });
+
+  fireEvent.change(screen.queryByLabelText(/Category/), {
+    target: { value: "Dessert" },
+  });
+
+  fireEvent.submit(screen.queryByText(/Add to List/));
+
+  expect(screen.queryByText(/Ice Cream/)).toBeInTheDocument();
+
+  expect(screen.queryAllByText(/Dessert/).length).toBe(dessertCount + 1);
+});
